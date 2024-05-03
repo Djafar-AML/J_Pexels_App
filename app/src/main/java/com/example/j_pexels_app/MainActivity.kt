31cbe22be838afa2277bcaca58e245273f91971b
Jeff <Jafaragha77@gmail.com>
2024-05-03 21:30:17 +0330

    - WindowCompat added

--- app/src/main/java/com/example/j_pexels_app/MainActivity.kt
+++ app/src/main/java/com/example/j_pexels_app/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.example.j_pexels_app.ui.theme.J_Pexels_AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
