d67aedc6b46a222b16398cc377001dc0fd6b6318
Jeff <Jafaragha77@gmail.com>
2024-05-07 13:36:07 +0330

    - PexelsConsts.kt added

---
+++ app/src/main/java/com/example/j_pexels_app/constants/PexelsConsts.kt
@@ -0,0 +1,12 @@
+package com.example.j_pexels_app.constants
+
+object PexelsConsts {
+
+    //    todo these are just for test purpose- better solution is to not include in production
+//    private const val HEADER_TOKEN = "bIC2Juk5eIDirNhbaW8S76xkIBHtxxbSgl4Wz1IUotWCA1i6KxpiWzre"
+//    private const val HEADER_TOKEN = "i9dqsauuLpBvvLf3Dq5CLEIGyrK3ILaw6d8oPmzbj62eawGm7eQfNCYG"
+    const val HEADER_TOKEN = "fpM0PPVA3zXke26GWY3u1OFPgDEaEzMZ9jnQRsLHHRURHRcdt51LIsPY"
+
+    const val TIMEOUT_RETROFIT = 30L
+
+}
