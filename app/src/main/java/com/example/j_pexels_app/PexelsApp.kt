c243a3946c732612e281213f9b979da7bf47265b
Jeff <Jafaragha77@gmail.com>
2024-05-07 13:30:19 +0330

    - Coil cache logic added to PexelsApp.kt

--- app/src/main/java/com/example/j_pexels_app/PexelsApp.kt
+++ app/src/main/java/com/example/j_pexels_app/PexelsApp.kt
@@ -1,7 +1,29 @@
 package com.example.j_pexels_app
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PexelsApp : Application()
+class PexelsApp : Application(), ImageLoaderFactory {
+    override fun newImageLoader() = imageLoader()
+
+    private fun imageLoader() = ImageLoader.Builder(context = this)
+        .crossfade(enable = true)
+        .memoryCache {
+            MemoryCache.Builder(context = this)
+                .maxSizePercent(percent = 0.25)
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(directory = cacheDir.resolve("coil_cache"))
+                .maxSizePercent(percent = 0.02)
+                .build()
+        }
+        .build()
+
+}
